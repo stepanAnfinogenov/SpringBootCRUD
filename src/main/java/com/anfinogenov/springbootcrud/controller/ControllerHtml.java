4db2ac4eadacfd864e904fee13e5ec6d311d57a2
stepan <stepan.anfinogenov@gmail.com>
2022-12-18 15:04:42 +0000

    add test Jenkinsfile

---
+++ src/main/java/com/anfinogenov/springbootcrud/controller/ControllerHtml.java
@@ -0,0 +1,20 @@
+package com.anfinogenov.springbootcrud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerHtml {
+
+    @RequestMapping("/")
+    public String defaultPage1 () {
+        return "<!DOCTYPE html><html><head>TEXT1<br></head><body>TEXT2</body></html>";
+    }
+
+    @RequestMapping("/2")
+    public String defaultPage2 () {
+        return "<!DOCTYPE html><html><head>TEXT1!!!!!!!!!!!!<br><br><br><br>" +
+                "</head><body>TEXT2%%%%%%%%%%%%%%<br><br><br><br>" +
+                "TEXT3&&&&&&&&&&&</body></html>";
+    }
+}
--- src/main/java/com/anfinogenov/springbootcrud/dao/EmployeeDaoImpl.java
+++ src/main/java/com/anfinogenov/springbootcrud/dao/EmployeeDaoImpl.java
@@ -2,12 +2,13 @@ package com.anfinogenov.springbootcrud.dao;
 
 import com.anfinogenov.springbootcrud.entity.Employee;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -17,9 +18,12 @@ public class EmployeeDaoImpl implements EmployeeDao{
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
 
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
@@ -27,23 +31,31 @@ public class EmployeeDaoImpl implements EmployeeDao{
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(employee);
 
-        session.saveOrUpdate(employee);
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     public Employee getEmployees(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = session.get(Employee.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//        Employee employee = session.get(Employee.class, id);
+
+        Employee employee = entityManager.find(Employee.class, id);
 
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+
+        Query query = entityManager.createQuery("delete from Employee where id =:employeeId");
         query.setParameter("employeeId", id);
         query.executeUpdate();
     }
