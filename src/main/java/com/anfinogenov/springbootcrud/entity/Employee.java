46c1f8045ca65151b4e2a77936c20e731407d66f
stepan <stepan.anfinogenov@gmail.com>
2023-01-12 10:58:00 +0000

    Added Lombok

--- src/main/java/com/anfinogenov/springbootcrud/entity/Employee.java
+++ src/main/java/com/anfinogenov/springbootcrud/entity/Employee.java
@@ -2,9 +2,15 @@ package com.anfinogenov.springbootcrud.entity;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "employees")
 @ApiModel(description = "Details about the Employee")
@@ -30,54 +36,4 @@ public class Employee {
     @Column(name = "salary")
     @ApiModelProperty(notes = "The Employee's salary")
     private int salary;
-
-    public Employee() {
-    }
-
-    public Employee(String name, String surname, String department, int salary) {
-        this.name = name;
-        this.surname = surname;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
 }
