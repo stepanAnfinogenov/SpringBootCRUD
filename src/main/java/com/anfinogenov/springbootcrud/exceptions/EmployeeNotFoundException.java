0b6e217c927b6fa83e8c4e322e69dca28f728036
stepan <stepan.anfinogenov@gmail.com>
2023-01-13 17:05:54 +0000

    Init

--- src/main/java/com/anfinogenov/springbootcrud/exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.anfinogenov.springbootcrud.exceptions;
-
-public class EmployeeNotFoundException extends Exception {
-  public EmployeeNotFoundException(String message) {
-    super(message);
-  }
-}
